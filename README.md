a90c9843215bd1678c2287963f40fb431715e654
Diliara <novoobrashennaya@yandex.ru>
2026-05-02 22:52:31 +0300

    feat: initial electron configs

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# BackToSchool_Front
+# Back To School
+
+Software for interactive information kiosks in schools.
+
+## Project Setup
+
+### Install
+
+```bash
+$ npm install
+```
+
+### Development
+
+```bash
+$ npm run dev
+```
+
+### Build
+
+```bash
+# For windows
+$ npm run build:win
+
+# For macOS
+$ npm run build:mac
+
+# For Linux
+$ npm run build:linux
+```
---
+++ electron.vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'path'
+import { defineConfig } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  main: {
+    build: {
+      externalizeDeps: true
+    },
+    resolve: {
+      alias: {
+        '@shared': resolve('src/shared')
+      }
+    }
+  },
+  preload: {
+    build: {
+      externalizeDeps: true
+    }
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared')
+      }
+    },
+    plugins: [react()]
+  }
+})
