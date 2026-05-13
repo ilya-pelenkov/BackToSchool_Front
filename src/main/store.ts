c1c7f188216082787fdd3ceeefde482c77d174f8
Diliara <novoobrashennaya@yandex.ru>
2026-05-13 10:49:41 +0300

    feat: electron store and electron logger

---
+++ src/main/store.ts
@@ -0,0 +1,16 @@
+import Store from 'electron-store'
+
+interface StoreSchema {
+  isRegistered: boolean
+  deviceId: string | null
+}
+
+//TODO: заменить store.set на реальную логику регистрации
+const store = new Store<StoreSchema>({
+  defaults: {
+    isRegistered: false,
+    deviceId: null,
+  },
+})
+
+export default store
