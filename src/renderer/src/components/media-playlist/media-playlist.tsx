68f4516b42941bbcbbff0285e0165286eec40bbd
Diliara <novoobrashennaya@yandex.ru>
2026-06-10 20:52:00 +0300

    feat: handle content errors and empty state in MediaPlaylist

--- src/renderer/src/components/media-playlist/media-playlist.tsx
+++ src/renderer/src/components/media-playlist/media-playlist.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useRef, useState } from 'react'
+import { Navigate, useNavigate } from 'react-router'
 
 import { TMediaIpcGetFiles } from '@shared/types'
+
+import { ROUTES } from '@renderer/app/router/routes'
 
 import { MediaPlayer } from '../media-player'
 import { Modal, QrButton } from '../ui'
@@ -8,12 +11,24 @@ import { Modal, QrButton } from '../ui'
 export function MediaPlaylist() {
   const [files, setFiles] = useState<TMediaIpcGetFiles>([])
   const [currentIndex, setCurrentIndex] = useState(0)
-  const errorCountRef = useRef(0)
   const [isPaused, setIsPaused] = useState(false)
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const errorCountRef = useRef(0)
+  const navigate = useNavigate()
 
   const loadFiles = (): void => {
-    window.api.media.getFiles().then(setFiles) //TODO: обработка ошибки
+    window.api.media
+      .getFiles()
+      .then(setFiles)
+      .catch(err => {
+        //ошибка маловероятна, но если что показывается экран отсутствия контента
+        console.error('Failed to load media files', err)
+        setFiles([])
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
   }
 
   useEffect(() => {
@@ -28,9 +43,12 @@ export function MediaPlaylist() {
   }
 
   const handleError = (): void => {
+    console.error('Failed to play media file', currentFile)
     errorCountRef.current += 1
     if (errorCountRef.current >= files.length) {
-      console.error('Нет воспроизводимых файлов в папке cached') //TODO: продумать заглушку
+      console.error('No playable files available')
+      window.api.media.requestForceSync() // запрашиваем перезагрузку файлов в main
+      navigate(ROUTES.noContent) // потом навигация
       return
     }
     goNext()
@@ -46,7 +64,8 @@ export function MediaPlaylist() {
     setIsModalOpen(false)
   }
 
-  if (!files.length) return <p>Нет файлов для воспроизведения</p> //TODO: продумать заглушку
+  if (isLoading) return null // перед проверкой !files.length чтобы избежать лишнего редиректа
+  if (!files.length) return <Navigate to={ROUTES.noContent} replace />
   const currentFile = files[currentIndex]
   const nextFile = files[(currentIndex + 1) % files.length]
 
